e4620d19977d599a48d869d79d41e44c4f230e43
Kavita Aherwar <37935155+Introverted27@users.noreply.github.com>
2018-07-19 12:57:36 +0530

    Pipfile and heroku settings added

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -94,9 +96,11 @@ DATABASES = {
     }
 }
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
+DATABASES['default']['CONN_MAX_AGE'] = 500
 DATABASES['default'].update(db_from_env)
+
+
 
 
 # Password validation
@@ -158,3 +162,6 @@ REST_FRAMEWORK = {
          'rest_framework.permissions.IsAuthenticated',
     )
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
